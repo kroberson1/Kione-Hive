fd075b52bdcf81057d2bee52c34dfaf80c9112ff
Code HQ <codeheadq@gmail.com>
2023-06-18 15:33:44 +0430

    made add task dialog scrollable

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,30 +1,41 @@
 class Task {
-  bool isChecked;
-  final String? id;
-  final String title;
-  final String description;
-  final DateTime? dateCreated;
+  import 'package:hive/hive.dart';
 
-  Task({
-    required this.id,
-    required this.title,
-    this.isChecked = false,
-    required this.description,
-    required this.dateCreated,
-  });
+  part 'task.g.dart';  // red unline is OK for now
 
-  @override
-  bool operator ==(covariant Task other) =>
-      id == other.id &&
-      title == other.title &&
-      description == other.description;
+  @HiveType(typeId: 0)
+  class Task {
+    @HiveField(0)
+    bool isChecked;
+    @HiveField(1)
+    final String? id;
+    @HiveField(2)
+    final String title;
+    @HiveField(3)
+    final String description;
+    @HiveField(4)
+    final DateTime? dateCreated;
 
-  @override
-  int get hashCode => Object.hashAll([
-        id,
-        title,
-        description,
-      ]);
-}
+    Task({
+      required this.id,
+      required this.title,
+      this.isChecked = false,
+      required this.description,
+      required this.dateCreated,
+    });
 
-List<Task> tasks = [];
+    @override
+    bool operator ==(covariant Task other) =>
+        id == other.id &&
+            title == other.title &&
+            description == other.description;
+
+    @override
+    int get hashCode => Object.hashAll([
+      id,
+      title,
+      description,
+    ]);
+  }
+
+  //List<Task> tasks = [];
